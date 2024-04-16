f88c8832b5e8b6bc12ba6c7be6c94a3e80cd4bc4
Ilya Usanov <b3er@who.ms>
2024-04-16 15:12:42 +0300

    refactor: update snackbar display

--- shared/src/commonMain/kotlin/com/github/b3er/cmp/issues/issue/WrongMultilineSnackbarPadding.kt
+++ shared/src/commonMain/kotlin/com/github/b3er/cmp/issues/issue/WrongMultilineSnackbarPadding.kt
@@ -2,13 +2,12 @@ package com.github.b3er.cmp.issues.issue
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.github.b3er.cmp.issues.Issue
 import com.github.b3er.cmp.issues.component.IssueScaffold
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 @Composable
@@ -30,30 +29,31 @@ fun Issue.WrongMultilineSnackbarPadding(
     ) {
         Column(modifier = Modifier.align(Alignment.Center)) {
             val scope = rememberCoroutineScope()
-            Button(onClick = {
-                scope.launch {
-                    snackbarHostState.showSnackbar("This is a snackbar")
+            var snackbarJob by remember { mutableStateOf<Job?>(null) }
+            fun showSnackbar(text: String) {
+                snackbarJob?.cancel()
+                snackbarJob = scope.launch {
+                    snackbarHostState.showSnackbar(text)
                 }
+            }
+            Button(onClick = {
+                showSnackbar("This is a snackbar")
             }) {
                 Text("Show 1 line snackbar")
             }
             Button(onClick = {
-                scope.launch {
-                    snackbarHostState.showSnackbar(
-                        "This is a 2 snackbar\nthat " +
-                            "should have no issues"
-                    )
-                }
+                showSnackbar(
+                    "This is a 2 snackbar\nthat " +
+                        "should have no issues"
+                )
             }) {
                 Text("Show 2 line snackbar")
             }
             Button(onClick = {
-                scope.launch {
-                    snackbarHostState.showSnackbar(
-                        "This is a multiline snackbar\nthat " +
-                            "should have issues\nwith padding on the bottom"
-                    )
-                }
+                showSnackbar(
+                    "This is a multiline snackbar\nthat " +
+                        "should have issues\nwith padding on the bottom"
+                )
             }) {
                 Text("Show 3 line snackbar")
             }
